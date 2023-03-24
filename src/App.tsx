c1d78e944c3bb47d5a9ef449d1116587e5938ca4
moisesdurandelrazo <moises6737@gmail.com>
2023-03-23 23:53:56 -0600

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 /** @format */
 
-import React, { MouseEventHandler } from "react";
+import React, { ChangeEventHandler, MouseEventHandler } from "react";
 import Button from "./components/Button";
+import Input from "./components/Input";
 import "bulma/css/bulma.css";
 import "./App.css";
 
 function App() {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.value);
+  };
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
     console.log("Hola mundo");
   };
   return (
     <div className="App">
+      <Input placeholder="Nombre" handleChange={handleChange} />
       <Button handleClick={handleClick}>Hola mundo</Button>
     </div>
   );
---
+++ src/components/Input.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import React, { ChangeEventHandler } from "react";
+
+interface InputProps {
+  placeholder: string;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function Input({ placeholder, handleChange }: InputProps) {
+  return <input placeholder={placeholder} onChange={handleChange} />;
+}
