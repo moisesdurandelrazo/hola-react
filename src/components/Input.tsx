922103820519ff8a4262c893c4a9105e900988d6
moisesdurandelrazo <moises6737@gmail.com>
2023-03-24 07:23:21 -0600

    useState

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,8 +5,11 @@ import React, { ChangeEventHandler } from "react";
 interface InputProps {
   placeholder: string;
   handleChange: ChangeEventHandler<HTMLInputElement>;
+  name: string;
 }
 
-export default function Input({ placeholder, handleChange }: InputProps) {
-  return <input placeholder={placeholder} onChange={handleChange} />;
+export default function Input({ placeholder, handleChange, name }: InputProps) {
+  return (
+    <input name={name} placeholder={placeholder} onChange={handleChange} />
+  );
 }
--- src/forms/User.tsx
+++ src/forms/User.tsx
@@ -1,22 +1,38 @@
 /** @format */
 
-import React, { ChangeEventHandler, MouseEventHandler } from "react";
+import React, { ChangeEventHandler, MouseEventHandler, useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 
+type UserFormState = {
+  name: string;
+  lastname: string;
+};
+
+const InitialValue: UserFormState = {
+  name: "",
+  lastname: "",
+};
 export default function UserForm() {
+  const [form, setForm] = useState(InitialValue);
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const name = e.target.name as keyof UserFormState;
+    setForm({ ...form, [name]: e.target.value });
     console.log(e.target.value);
   };
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
-    console.log("Hola mundo");
+    console.log(form);
   };
 
   return (
     <>
-      <Input placeholder="Nombre" handleChange={handleChange} />
-      <Input placeholder="Apellido" handleChange={handleChange} />
+      <Input name="name" placeholder="Nombre" handleChange={handleChange} />
+      <Input
+        name="lastname"
+        placeholder="Apellido"
+        handleChange={handleChange}
+      />
       <Button handleClick={handleClick}>Hola mundo</Button>
     </>
   );
